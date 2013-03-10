4d6b625e98075ad24dd6d92a413484c371e0558b
Daniel J Sell <dan.j.sell@gmail.com>
2013-03-10 12:03:42 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrettySlim::Application.config.secret_token = 'cfac5ff827410ca2717c22c5585e1ef4d088912b6c59fdf4994206af8386cdc3fd91685ca1732b31205853105d163e04494e417a9ac42696bc7a37f18f13dd25'
